df810c10d6c6c217739a1c52f68d813e15ea321f
Fismaja <126912042+Fismaja@users.noreply.github.com>
2023-06-16 18:35:21 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,21 +1,22 @@
+// This is the content of the proxy.js file
 module.exports = async (req, res) => {
-  const fetch = require('node-fetch');
-  
-  const url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=GOV';
-  const apiKey = '01e0ee8f-b5bb-4ff8-8912-bc7e03b49dd8';
+    const fetch = require('node-fetch');
 
-  try {
-    const response = await fetch(url, {
-      headers: {
-        'X-CMC_PRO_API_KEY': apiKey
-      }
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+
+    const apiKey = '01e0ee8f-b5bb-4ff8-8912-bc7e03b49dd8';
+    const apiUrl = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=GOV';
+
+    const response = await fetch(apiUrl, {
+        headers: {
+            'X-CMC_PRO_API_KEY': apiKey
+        }
     });
-    
+
     const data = await response.json();
-    
+
     res.status(200).send(data);
-  } catch (error) {
-    console.log('Error fetching data from CoinMarketCap:', error);
-    res.status(500).send('Error fetching data from CoinMarketCap');
-  }
 };
