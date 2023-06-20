7accec372df8439dc25b00b72636aa542d1d461a
Fismaja <126912042+Fismaja@users.noreply.github.com>
2023-06-20 02:11:18 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 
 module.exports = async (req, res) => {
   try {
-    const response = await fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=GOV", {
+    const response = await fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=27016", {
       headers: {
         'X-CMC_PRO_API_KEY': '01e0ee8f-b5bb-4ff8-8912-bc7e03b49dd8'
       }
